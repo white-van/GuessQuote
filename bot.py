f33a8911521c8446148ba839f8300cec02015736
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2022-01-09 17:29:41 -0500

    add suggested changes

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import re
 from dotenv import load_dotenv
 load_dotenv()
 
-from quotes import name_alias, quotes
+from quotes import name_alias, quotes, id_to_name
 
 #The Discord Auth token
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -16,68 +16,95 @@ client = discord.Client()
 #Prefix
 prefix = '-'
 
+WRONG_ANSWERS_NEEDED = 4
+
 # The different playable games
-gameStates = {
-    'guessGame': {
+gameState = {
         'ans': None,
         'origQuote': None,
         'namelessQuote': None,
-    }
+        'hints': [],
+        'guesses': 0
 }
 
-leaderboards = {
-    'guessGame': {
+leaderboard = {
         'userIdToPoints': {},
         'leaderboard': []
-    }
 }
 
 
 #Display helper functions
 def DisplayHelp():
     embed=discord.Embed(title='Guess Quote Commands!', color=0x702eb2)
-    embed.add_field(name='{0}guessquote ({0}gq)'.format(prefix), value='Starts a quote guessing game!', inline=False)
-    embed.add_field(name='{}giveup'.format(prefix), value='Ends the current game', inline=False)
-    embed.add_field(name='{}leaderboard'.format(prefix), value='displays the current leaderboard', inline=False)
-    embed.add_field(name='{}quote'.format(prefix), value='Repeats current quote', inline=True)
+    embed.add_field(name=f'{prefix}guessquote ({prefix}gq)', value='Starts a quote guessing game!', inline=False)
+    embed.add_field(name=f'{prefix}giveup', value='Ends the current game', inline=False)
+    embed.add_field(name=f'{prefix}leaderboard', value='displays the current leaderboard', inline=False)
+    embed.add_field(name=f'{prefix}quote', value='Repeats current quote', inline=True)
+    embed.add_field(name=f'{prefix}hint', value=f'Displays some hints, can only be used after {WRONG_ANSWERS_NEEDED} wrong guesses', inline=True)
     return embed
 
-def DisplayGuessGame(ans = None, origQuote = None, namelessQuote = None):
+def DisplayGuessGame():
+    global gameState
+
+    namelessQuote = gameState['namelessQuote']
+
     embed=discord.Embed(title="Guess who said this quote!", color=0x702eb2)
     embed.add_field(name="‎", value=namelessQuote, inline=True)
     return embed
 
 def DisplayNoGame():
-    return discord.Embed(title="Oh no!", description="There currently isn't a game running, start one with {0}guessquote or {0}gq!".format(prefix), color=0x702eb2)
+    return discord.Embed(title="Oh no!", description=f'There currently isn\'t a game running, start one with {prefix}guessquote or {prefix}gq!', color=0x702eb2)
 
-def DisplayGiveup(ans = None, origQuote = None, namelessQuote = None):
+def DisplayGiveup():
+    global gameState
+
+    origQuote = gameState['origQuote']
+
     embed=discord.Embed(title="So close!", description="You weren't able to guess the quote, that sucks!", color=0x702eb2)
     embed.add_field(name="Original Quote", value=origQuote, inline=True)
     return embed
 
-def DisplayCorrect(ans = None, origQuote = None, namelessQuote = None):
+def DisplayCorrect():
+    global gameState
+
+    origQuote = gameState['origQuote']
+
     embed=discord.Embed(title="Correct!", description="Spot on, you got it right!", color=0x702eb2)
     embed.add_field(name="Original Quote", value=origQuote, inline=True)
     return embed
 
-def DisplayIncorrect():
-    embed=discord.Embed(title="Guess the Quote!", description="Incorrect, ya got the wrong person!", color=0x702eb2)
-    return embed
-
 async def DisplayLeaderboard(clientUser):
     embed=discord.Embed(title="Leaderboard!", color=0x702eb2)
-    curLeaderboard = leaderboards['guessGame']
-    if curLeaderboard['userIdToPoints'].get(clientUser, None) != None:
-        theirRank = [cliId for cliId, _ in curLeaderboard['leaderboard']].index(clientUser) + 1
+    if leaderboard['userIdToPoints'].get(clientUser, None) != None:
+        theirRank = [cliId for cliId, _ in leaderboard['leaderboard']].index(clientUser) + 1
         embed.add_field(name="Your Rank!",value=theirRank, inline= False)
-        embed.add_field(name="Your Points!",value=curLeaderboard['leaderboard'][theirRank - 1][1], inline = False)
+        embed.add_field(name="Your Points!",value=leaderboard['leaderboard'][theirRank - 1][1], inline = False)
 
-    if len(curLeaderboard['leaderboard']) != 0:
+    if len(leaderboard['leaderboard']) != 0:
         counter = 1
-        for leader, value in curLeaderboard['leaderboard'][:10]:
+        for leader, value in leaderboard['leaderboard'][:10]:
             leaderName = await client.fetch_user(leader)
-            embed.add_field(name='{}. {}'.format(counter,leaderName), value=value, inline = False)
+            embed.add_field(name=f'{counter}. {leaderName}', value=value, inline = False)
             counter += 1
+    return embed
+
+def DisplayHint():
+    global gameState
+    embed=discord.Embed(title="Here's a hint for ya!", color=0x702eb2)
+    embed.set_footer(text=f'you can bring up this hint again with {prefix}hint')
+
+    hints = gameState['hints']
+    for index,hint in enumerate(hints):
+        embed.add_field(name=f'{index + 1}. {hint}',value="‎", inline= False)
+    return embed
+
+def DisplayKeepGuessing():
+    global gameState
+
+    guesses = gameState['guesses']
+    embed= discord.Embed(title=f'Keep guessing!', color=0x702eb2)
+
+    embed.add_field(name=f"You need {WRONG_ANSWERS_NEEDED - guesses} more to unlock this command", value="‎")
     return embed
 
 
@@ -91,10 +118,40 @@ def has_match(lstOne, lstTwo):
             return True 
     return False 
 
+def get_mc_choices(numChoicesNeeded, ansUser):
+    listOfIds = random.sample(id_to_name.keys(),numChoicesNeeded)
+
+    isAlreadyIn = False
+    newNameChoices = []
+    for curId in listOfIds:
+        names = id_to_name[curId]
+        if ansUser in names and not isAlreadyIn:
+            newNameChoices.append(ansUser)
+            isAlreadyIn = True
+        else:
+            newNameChoices.append(random.choice(list(names)))
+    
+    if isAlreadyIn:
+        return newNameChoices
+    else:
+        random_index = random.randint(0,len(newNameChoices)-1)
+        
+        if random_index == 0:
+            return [ansUser] + newNameChoices[1:]
+        elif random_index == len(newNameChoices) - 1:
+            return newNameChoices[:-1] + [ansUser]
+        return newNameChoices[:random_index] + [ansUser] + newNameChoices[random_index + 1:]
+
+
 #Game one
 
-def parseGameOne(message, clientUser, ans, origQuote, namelessQuote):
+async def parseGameOne(messageObj, message, clientUser):
     global currentGame
+    global gameState
+
+    ans = gameState['ans']
+    guesses = gameState['guesses']
+    
     name = None 
     messageList = message.split(' ')
     if len(messageList)  > 1:
@@ -102,44 +159,57 @@ def parseGameOne(message, clientUser, ans, origQuote, namelessQuote):
     #check if they try to initiate here
     if message in ['gq','guessquote']:
         #Trying to be sneaky
-        return discord.Embed(title='Oh no!', description='You tried to start a new game, when a current game is already in session! Use -quote to repeat the quote, or -giveup to quit the game!', color=0x702eb2)
+        return discord.Embed(title='Oh no!', description='A game is already in progress! Use -guess to guess or -giveup to quit the game. Quote: {namelessQuote}', color=0x702eb2)
     elif message in ['quote']:
         #Repeat Quote
-        return DisplayGuessGame(namelessQuote = namelessQuote)
+        return DisplayGuessGame()
     elif message in ['giveup']:
         #Gave up
         currentGame = 0
         resetGameOne()
-        return DisplayGiveup(origQuote = origQuote)
+        return DisplayGiveup()
+    elif message in ['hint']:
+        if guesses >= WRONG_ANSWERS_NEEDED:
+            return DisplayHint()
+        else:
+            return DisplayKeepGuessing()
     elif ans.lower() in message.lower() or (name_alias.get(name, None) != None and has_match(name_alias[name], name_alias[ans.lower()])):
         #Winner!
-        curLeaderboard = leaderboards['guessGame']
-        curLeaderboard['userIdToPoints'][clientUser] = curLeaderboard.get(clientUser, 0) + 1
-        rankings = list(curLeaderboard['userIdToPoints'].items())
+        leaderboard['userIdToPoints'][clientUser] = leaderboard.get(clientUser, 0) + 1
+        rankings = list(leaderboard['userIdToPoints'].items())
         rankings.sort(key=lambda tup: tup[1], reverse=True)
-        curLeaderboard['leaderboard'] = rankings
+        leaderboard['leaderboard'] = rankings
 
         #Rest state
         currentGame = 0
+        embed = DisplayCorrect()
         resetGameOne()
-        return DisplayCorrect(origQuote = origQuote)
+        await messageObj.add_reaction('✅')
+        return embed
     else:
         #Wrong Guess
-        return DisplayIncorrect()
+        await messageObj.add_reaction('❌')
+
+        gameState['guesses'] += 1
+        if (gameState['guesses'] == WRONG_ANSWERS_NEEDED):
+            return DisplayHint()
 
 
 def resetGameOne():
-    gameStates["guessGame"] = {
+    global gameState
+    gameState = {
         'ans': None,
         'origQuote': None,
         'namelessQuote': None,
+        'hints': [],
+        'guesses': 0
     }
     
 #The current game playing, 0 if none
 currentGame = 0
 
 
-parseGameFunc = [None, ('guessGame',parseGameOne)]
+parseGameFunc = [None, parseGameOne]
 
 resetGameFunc = [None, resetGameOne]
 
@@ -162,7 +232,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     global currentGame
-    global gameStates
+    global gameState
     if message.author == client.user:
         return
 
@@ -180,25 +250,27 @@ async def on_message(message):
         elif messageContent in ['leaderboard', 'lb']:
             response = await DisplayLeaderboard(clientUser)
         
-            # Prevents starting a new game if a game is on progress
         elif currentGame != 0: 
-            key, curGameParser = parseGameFunc[currentGame]
-            response= curGameParser(messageContent,clientUser, **gameStates[key])
+            curGameParser = parseGameFunc[currentGame]
+            response= await curGameParser(message, messageContent,clientUser)
         else:
             #Start the game
             if (messageContent in ['guessquote','gq']):
                 quoteText, quoteAuthor, OGQuote = randquote()
+                hints = get_mc_choices(4,quoteAuthor)
                 currentGame = 1
-                gameStates['guessGame'] = {
+                gameState= {
                     'ans': quoteAuthor,
                     'origQuote': OGQuote,
-                    'namelessQuote': quoteText
+                    'namelessQuote': quoteText,
+                    'hints': hints,
+                    'guesses': 0
                 }
-                response = DisplayGuessGame(**gameStates['guessGame'])
-            elif (messageContent in ['giveup', 'quote','guess']):
+                response = DisplayGuessGame()
+            elif (messageContent in ['giveup', 'quote','guess','hint']):
                 response = DisplayNoGame()
 
         if response != None:
-            await message.channel.send(embed = response)
+            await message.reply(embed = response, mention_author = False)
 
 client.run(TOKEN)
